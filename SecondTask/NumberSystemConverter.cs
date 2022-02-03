e26fae2087d723722e6720f01286bc2c0e385432
Nuretdin Meredov <meredov.nurik00@gmail.com>
2022-02-03 22:34:24 +0300

    Pre-Alpha Second Task

---
+++ SecondTask/NumberSystemConverter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondTask
+{
+    public class NumberSystemConverter
+    {
+        const int minimumBasis = 2;
+        const int maximumBasis = 20;
+        public string BasisConverter(int number, int newBasis)
+        {
+            try
+            {
+                bool negative = false;
+                if (newBasis < minimumBasis || newBasis > maximumBasis)
+                {
+                    throw new Exception("New Basis must be less than 20 and bigger than 2");
+                }
+                StringBuilder numberInNewBasis = new StringBuilder();
+                if(number < 0)
+                {
+                    negative = true;
+                    number = Math.Abs(number);
+                }
+                while (number != 0)
+                {
+                    int remainder = number % newBasis;
+                    if (remainder < 10)
+                    {
+                        numberInNewBasis.Append(remainder);
+                    }
+                    else
+                    {
+                        numberInNewBasis.Append((char)(55 + remainder)); //65 in ASCII table is symbol`s A code
+                    }
+                    number /= newBasis;
+                }
+                if (negative)
+                {
+                    numberInNewBasis.Append('-');
+                }
+
+                return new string (numberInNewBasis.ToString().Reverse().ToArray());
+            }
+            catch (Exception e)
+            {
+                return e.Message.ToString();
+            }
+        }
+    }
+}
---
+++ SecondTask/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SecondTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 2)
+            {
+                int.TryParse(args[0], out int number);
+                int.TryParse(args[1], out int newBasis);
+
+                NumberSystemConverter Converter = new NumberSystemConverter();
+                string result = Converter.BasisConverter(number, newBasis);
+                Console.WriteLine(result);
+
+            }
+        }
+    }
+}
